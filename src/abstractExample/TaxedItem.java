8f5fa949cbfb9de095df50ae2e28dec7de93d3ab
philo-nabil <123249336+philo-nabil@users.noreply.github.com>
2026-03-22 12:41:04 +0200

    TaxedItem extends OrderItem with 14% tax
    Change TaxedItem to extend OrderItem, add a constructor that delegates name and basePrice to super, and override getFinalPrice() to apply a 14% tax (basePrice + basePrice * 0.14) using getBasePrice().

--- src/abstractExample/TaxedItem.java
+++ src/abstractExample/TaxedItem.java
@@ -1,4 +1,14 @@
 package abstractExample;
 
-public class TaxedItem {
+public class TaxedItem extends OrderItem {
+
+    public TaxedItem(String name, double basePrice) {
+        super(name, basePrice);
+    }
+
+
+    @Override
+    double getFinalPrice() {
+        return getBasePrice() + getBasePrice() * 0.14;
+    }
 }
